2d86554b6b3758bc1193e95542ac833fb711fb34
tulen <zlobinaanastasiy@yandex.ru>
2025-05-06 16:22:20 +0300

    Добавление элементов управления на форму

---
+++ salary-analysis/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace salary_analysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(347, 210);
+            button1.Name = "button1";
+            button1.Size = new Size(142, 56);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(329, 91);
+            button2.Name = "button2";
+            button2.Size = new Size(164, 77);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(329, 36);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(160, 27);
+            numericUpDown1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(351, 296);
+            button3.Name = "button3";
+            button3.Size = new Size(138, 53);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private NumericUpDown numericUpDown1;
+        private Button button3;
+    }
+}
