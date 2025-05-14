4116314724a6223a8bfe4503ee0b4b0dd99f335a
pechenikovaa <ms.annet01@gmail.com>
2025-05-14 15:13:35 +0300

    Слияние веток

--- salary-analysis/obj/Debug/net8.0-windows/salary-analysis.AssemblyInfo.cs
+++ salary-analysis/obj/Debug/net8.0-windows/salary-analysis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("salary-analysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09f92af8dac0c5a1afaee1c4906f501626c688b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1beccb4f36068fd7e38b29fb21fe6dc6df6acdde")]
 [assembly: System.Reflection.AssemblyProductAttribute("salary-analysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("salary-analysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
