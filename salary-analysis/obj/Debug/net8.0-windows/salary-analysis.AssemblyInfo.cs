a566bbce567daa971c18068a217ab11e1be2e770
pechenikovaa <ms.annet01@gmail.com>
2025-05-15 12:56:03 +0300

    Решение конфликтов

--- salary-analysis/obj/Debug/net8.0-windows/salary-analysis.AssemblyInfo.cs
+++ salary-analysis/obj/Debug/net8.0-windows/salary-analysis.AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d6bba2d9e0203fed4ad53ce7551533b50184af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f494c234881a83a288afe5b9c2f657c859268e")]
 [assembly: System.Reflection.AssemblyProductAttribute("salary-analysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("salary-analysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
