cd2e42174e376172040248b5387f414c41c6c08f
tulen <zlobinaanastasiy@yandex.ru>
2025-05-15 12:34:52 +0300

    Сохранение файлов для отключения ошибки

--- salary-analysis/obj/Debug/net8.0-windows/salary-analysis.AssemblyInfo.cs
+++ salary-analysis/obj/Debug/net8.0-windows/salary-analysis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("salary-analysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1beccb4f36068fd7e38b29fb21fe6dc6df6acdde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f35afb2e006bd794a1cae875a34580991128b917")]
 [assembly: System.Reflection.AssemblyProductAttribute("salary-analysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("salary-analysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
